f9297444054dfb3ae72d3a4806465aacade78404
Mukeshp405 <mukeshp4287@gmail.com>
2024-07-06 21:48:23 +0530

    file add

---
+++ program3.py
@@ -0,0 +1,19 @@
+# TypeCasting
+
+a = "3"
+b = "2"
+c = 4
+d = None
+e = True
+f = 3.4
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print("")
+print("Interger :-", int(a) + int(b))
+print("String :-", str(a) + str(b))
+print("Float :-" ,float(a) + float(b))
---
+++ program4.py
@@ -0,0 +1,11 @@
+# Taking input from user
+a = input("Enter the first number :- ")
+b = input("Enter the second number :- ")
+
+print("---------------------")
+print("Addition :- ", int(a) + int(b))
+print("Subtraction :- ", int(a) - int(b))
+print("Multiplication :- ", int(a) * int(b))
+print("Division :- ", int(a) / int(b))
+print("Modulus :- ", int(a) % int(b))
+print("---------------------")
