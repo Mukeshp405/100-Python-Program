190640177424a18497987e1127eae1a6ed1cd0c7
Mukeshp405 <mukeshp4287@gmail.com>
2024-07-06 13:38:02 +0530

    file add

---
+++ program2.py
@@ -0,0 +1,11 @@
+num1 = 2
+num2 = 3
+
+# Arithmetic Operators
+print("The Addition of", num1 , "and",  num2 , "is :-", num1 + num2)
+print("The Subtraction of", num1 , "and",  num2 , "is :-", num1 - num2)
+print("The Multiplication of", num1 , "and",  num2 , "is :-", num1 * num2)
+print("The Division of", num1 , "and",  num2 , "is :-", num1 / num2)
+print("The Modulus of", num1 , "and",  num2 , "is :-", num1 % num2)
+print("The Exponentiation of", num1 , "and",  num2 , "is :-", num1 ** num2)
+print("The Floor division of", num1 , "and",  num2 , "is :-", num1 // num2) 
